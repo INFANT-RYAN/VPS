2f7d04731ce90187fe37d48be793e68c3bcecddc
Infant-Ryan <ini0k17@gmail.com>
2024-12-29 18:17:59 +0530

    Fix: desktop-only site

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import Header from "./Componants/Header";
 
 export default function App() {
   return (
+    <>
     <section className="hidden lg:block">
-    <Header></Header>
-    <div className="block lg:hidden"> <h1 className="text-center text-2xl p-4"> Sorry, this website is only available on desktop. </h1></div>
+      <Header></Header>
     </section>
+    <section className="block lg:hidden "> <h1 className="text-center text-2xl p-4"> Sorry, this website is only available on desktop. </h1></section>
+    </>
   )
 }

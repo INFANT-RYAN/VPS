c10a4405e07c4af39b09789d29694abc17eb1098
Infant-Ryan <ini0k17@gmail.com>
2024-12-29 15:33:43 +0530

    fix: topbar oadding adj

--- src/Componants/Topbar.jsx
+++ src/Componants/Topbar.jsx
@@ -14,12 +14,12 @@ export default function Topbar(){
     return(
         <>
         <div className="bg-[#9874f2] text-sm font-Tb text-white flex justify-between p-3 mx-auto container">
-            <ul className=" flex gap-2">
+            <ul className=" flex gap-6">
                 <li className="flex items-center gap-1"><IoLocationSharp className=""/><p>30, 3rd Cross-Mariamman Nagar, Mariamman Nagar, Mudaliarpet, Puducherry, 605004</p></li>
                 <li className="flex items-center gap-1"><FaPhone /><p>8903369700</p></li>
                 <li className="flex items-center gap-1"><MdEmail /><p>ini0k17@gmail.com</p></li>
             </ul>
-            <ul className="flex gap-2">
+            <ul className="flex gap-3">
                 <li>Follow Us On : </li>
                 <FaFacebookF />
                 <FaTwitter />

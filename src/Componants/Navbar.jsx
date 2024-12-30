eb82bd1db130c237b42326284e821529ad20dc59
Infant-Ryan <ini0k17@gmail.com>
2024-12-30 11:12:51 +0530

    fix: font correction barlow

--- src/Componants/Navbar.jsx
+++ src/Componants/Navbar.jsx
@@ -3,9 +3,9 @@ import logo from "../assets/logo.png"
 export default function Navbar(){
     return(
         <>
-        <nav className="bg-[#e0eaff] flex gap-5 items-center justify-center mx-auto container">
+        <nav className="bg-[#e0eaff] flex gap-5 items-center justify-center mx-auto container font-barlow">
             <img src={logo} alt="logo" srcset="" className="h-32 w-32" />
-            <ul className="flex gap-4 text-[#141d38] font-semibold font-nav">
+            <ul className="flex gap-4 text-[#141d38]  font-semibold ">
                 <li><a href="#">HOME</a></li>
                 <li><a href="#">IT SERVICES</a></li>
                 <li><a href="#">COURSES</a></li>
